398a792033d5f9ca7f75e2206dec7a34ac78bdbf
Lugsole <Lugsole@users.noreply.github.com>
2020-09-16 16:51:44 -0400

    added example that uses RPN

---
+++ src/chart_command_line/math.java
@@ -0,0 +1,66 @@
+package chart_command_line;
+
+import Chart_Components.Chart;
+import java.util.Scanner;
+import java.util.Stack;
+public class math {
+
+	public static void main(String[] args) {
+		Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+		System.out.println("Enter formula");
+		String mathFormula = myObj.nextLine();  // Read user input
+		String[] splited = mathFormula.split("\\s+");
+		Stack<Double> s = new Stack<Double>();
+		Stack<Chart> c = new Stack<Chart>();
+		for(String command : splited) {
+			if (command.equals("")){
+			}else if (command.equals("+")) {
+				Double a = s.pop();
+				Double b = s.pop();
+				s.push(Double.valueOf(b + a));
+				Chart new_box = new Chart(command);
+				Chart chart_a = c.pop();
+				new_box.add(c.pop());
+				new_box.add(chart_a);
+				c.push(new_box);
+			} else if( command.equals("-")){
+				Double a = s.pop();
+				Double b = s.pop();
+				s.push(Double.valueOf(b - a));
+				Chart new_box = new Chart(command);
+				Chart chart_a = c.pop();
+				new_box.add(c.pop());
+				new_box.add(chart_a);
+				c.push(new_box);
+			} else if (command.equals("*")) {
+				Double a = s.pop();
+				Double b = s.pop();
+				s.push(Double.valueOf(b * a));
+				Chart new_box = new Chart(command);
+				Chart chart_a = c.pop();
+				new_box.add(c.pop());
+				new_box.add(chart_a);
+				c.push(new_box);
+			} else if (command.equals("/")) {
+				Double a = s.pop();
+				Double b = s.pop();
+				s.push(Double.valueOf(b / a));
+				Chart new_box = new Chart(command);
+				Chart chart_a = c.pop();
+				new_box.add(c.pop());
+				new_box.add(chart_a);
+				c.push(new_box);
+			} else {
+				s.push(Double.valueOf(Double.parseDouble(command)));
+				c.push(new Chart(command));
+			}
+		}
+		while(!s.empty()){
+			System.out.println(s.pop());
+		}
+		while(!c.empty()){
+			System.out.println(c.pop());
+		}
+	}
+
+}
